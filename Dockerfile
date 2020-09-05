cb630bc8ba1e7f42ecd37c9a3da097a40f42c124
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-05 13:19:53 +0200

    Implemented new logging
    Removed need for ignore network

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /app
 
 COPY root /
 
+ENV NETWORK=''
+
 RUN apk add --update --no-cache tinyproxy
 
 EXPOSE 8888
--- root/app/tinyproxy/03-init.sh
+++ root/app/tinyproxy/03-init.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-if expr "${NETWORK}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
-    log -i "Adding route for communication with network $NETWORK/24";
+if expr "${NETWORK}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.0$' >/dev/null; then
+    log -i tinyproxy "Adding route for communication with network $NETWORK/24";
     route add -net ${NETWORK} netmask 255.255.255.0 gw $(ip route | awk '/default/ { print $3 }')
-elif [ "$NETWORK" = "ignore" ] ; then
-    log -w "Ignoring network routes. Probably won't be able to use tinyproxy."
 else
-    log -e "-e NETWORK missing or wrong format. Please see the docs @ https://hub.docker.com/r/rundqvist/openvpn-tinyproxy/";
-    exit 1;
+    log -w tinyproxy "NETWORK missing or wrong format. May cause communication problems.";
 fi
