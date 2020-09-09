da4c3a899bfc242eb2a376196ed06c522d564813
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-09 18:19:13 +0200

    Moved route adding to base container

--- root/app/tinyproxy/03-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if expr "${NETWORK}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.0$' >/dev/null; then
-    log -i tinyproxy "Adding route for communication with network $NETWORK/24";
-    route add -net ${NETWORK} netmask 255.255.255.0 gw $(ip route | awk '/default/ { print $3 }')
-else
-    log -w tinyproxy "NETWORK missing or wrong format. May cause communication problems.";
-fi
